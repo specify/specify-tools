5668131cb09e58a357104bdca2769fdccfa5de5c
Maksym Patiiuk <info@mambo.in.ua>
2020-04-07 10:58:17 -0500

    Improve xml_localization. Add changelog

--- file_gen.php
+++ file_gen.php
@@ -5,24 +5,40 @@ if(array_key_exists('count',$_GET))
 else
 	$count = 200;
 
+if(array_key_exists('name',$_GET))
+	$file_name = $_GET['name'];
+else
+	$file_name = '*_localities.csv';
+
+if(array_key_exists('type',$_GET) && in_array($_GET['type'],['locality','co']))
+	$type = $_GET['type'];
+else
+	$type = 'default';
+
 if(array_key_exists('to_file',$_GET))
 	$TO_FILE = $_GET['to_file']==1 || $_GET['to_file']=='true';
 else
 	$TO_FILE = false;
 
 //Config
-$FILE_NAME = $count.'_localities.csv';
-$cols = 5;
 $array = [];
 $all_rows_same = !true;
-$default_value = 1;
+$default_value = '';
 
+switch($type){
+	case 'locality': $cols=6; break;
+	case 'co': $cols=20; break;
+	default: $cols=10; break;
+}
+
+if(strlen($file_name)>0 && $file_name[0] == '*')
+	$file_name = $count.substr($file_name,1);
 
 //Output setup
 if($TO_FILE){
     header("Content-Description: File Transfer");
     header("Content-Type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=\"". $FILE_NAME ."\"");
+    header("Content-Disposition: attachment; filename=\"". $file_name ."\"");
 }
 
 
@@ -42,10 +58,26 @@ function custom_rand($min, $max, $div){
 //[func_name,[param1,param2,...]
 //'static_val'
 //'' - use def if defined. else use $i
-$array[1] = ['custom_rand', [-90000, 90000, 1000]];
-$array[2] = ['custom_rand', [-180000, 180000, 1000]];
-$array[3] = ['custom_rand', [-90000, 90000, 1000]];
-$array[4] = ['custom_rand', [-180000, 180000, 1000]];
+if($type == 'locality'){
+	$array[1] = ['custom_rand', [-90000, 90000, 1000]];
+	$array[2] = ['custom_rand', [-180000, 180000, 1000]];
+	$array[3] = ['custom_rand', [-90000, 90000, 1000]];
+	$array[4] = ['custom_rand', [-180000, 180000, 1000]];
+	$array[5] = 'Line';
+}
+elseif($type == 'co')
+	$array[0];
+else {
+	$array[1] = ['custom_rand', [1, 100, 1]];
+	$array[2] = ['custom_rand', [1, 100, 1]];
+	$array[3] = ['custom_rand', [1, 100, 1]];
+	$array[4] = ['custom_rand', [1, 100, 1]];
+	$array[5] = ['custom_rand', [1, 100, 1]];
+	$array[6] = ['custom_rand', [1, 100, 1]];
+	$array[7] = ['custom_rand', [1, 100, 1]];
+	$array[8] = ['custom_rand', [1, 100, 1]];
+	$array[9] = ['custom_rand', [1, 100, 1]];
+}
 
 
 //Output col names
