3b7b5934febaa8e7477da79e9c1cd53c07c4bb89
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2020-11-27 01:03:28 +0200

    Improves docker notifier

--- docker_container/common.py
+++ docker_container/common.py
@@ -1,16 +1,13 @@
 import subprocess
 import os
+import pync
 import time
-from playsound import playsound
 
 
-
-def send_notification(message, title, sound):
-	process = subprocess.Popen("osascript -e 'display notification \"%s\" with title \"%s\"'" % (message, title), shell=True)
-	process.wait()
-	playsound('/System/Library/Sounds/'+sound+'.aiff')
-
+def send_notification(message, title, sound='Pop'):
+        pync.notify(message, title=title, group='specify7_notifications')
+        run_process('afplay /System/Library/Sounds/%s.aiff' % sound)
 
 def run_process(command):
-	process = subprocess.Popen(command, shell=True)
-	process.wait()
+        os.system(command)
+
