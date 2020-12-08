e189a56af18626045347ac84c0f8d744532b9719
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2020-12-08 19:41:11 +0200

    Modifies the docker script to reflect container's changes

--- docker_container/update_full.py
+++
@@ -1,4 +0,0 @@
-from common import *
-
-run_process("docker cp /Users/mambo/site/py_charm/specify7/specifyweb specify7-docker_specify7_1:/usr/local/specify7/")
-send_notification('Updating (Full)...','Updating (Full)...')
