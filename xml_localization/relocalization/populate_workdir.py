7f29585ba21268881fad5ba50b12b5a85f223580
Max Patiiuk <max@patii.uk>
2021-03-01 04:04:52 +0200

    Add support for workbench schema localization file

--- xml_localization/relocalization/populate_workdir.py
+++ xml_localization/relocalization/populate_workdir.py
@@ -4,26 +4,30 @@ from shutil import copyfile
 from termcolor import colored
 from pathlib import Path
 
-for dir_path, _, filenames in os.walk(
-    os.path.join(
-        os.getcwd(),
-        config.specify6_location
-    )
-):
+base_path = os.path.join(
+    os.getcwd(),
+    config.specify6_location
+)
+
+for dir_path, _, filenames in os.walk(base_path):
     for filename in [
-        f for f in filenames if f == config.source_file_name
+        f for f in filenames if f == config.source_file_name or \
+                                f == config.wb_source_file_name
     ]:
         directory = dir_path.split('/')[-1]
 
         if directory == 'config':
-            directory = 'main'
+            if filename == config.source_file_name:
+                directory = 'main'
+            if filename == config.wb_source_file_name:
+                directory = 'workbench'
 
         source = os.path.join(dir_path, filename)
         destination = os.path.join(
                 os.getcwd(),
                 config.working_directory,
                 directory,
-                filename
+                config.source_file_name
             )
 
         print('%s %s %s' % (
