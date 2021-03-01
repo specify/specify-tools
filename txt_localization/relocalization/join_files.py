b2af5211c3a971800020146b98e8408eac25a084
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-01 03:21:33 +0200

    Update UI localization sources and scripts

---
+++ txt_localization/relocalization/join_files.py
@@ -0,0 +1,11 @@
+import glob
+
+final_content = ''
+
+for file_name in glob.glob('./properties_to_tsv/*.tsv'):
+    with open(file_name) as file:
+        content = file.read()
+    final_content += '~~~~~~~\n%s\n%s\n' % (file_name, content)
+
+with open('joined_files.tsv', 'w') as file:
+    file.write(final_content)
