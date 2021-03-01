b1688a31666e4ca91bfee4435b00da959c2cfa54
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-01 03:28:49 +0200

    Make `deconstruct` fix missing `en` strings for some keys automatically

---
+++ xml_localization/relocalization/error_fixers/pre_deconstruct.py
@@ -0,0 +1,15 @@
+from xml_localization.relocalization import config
+from xml_localization.relocalization.error_fixers import missing_en_key
+
+
+def error_callback(error_type, error_message):
+    if error_type == 'missing_en_key':
+        return missing_en_key.error_callback()
+    raise Exception(error_message)
+
+
+def fix(extracted_strings):
+    if config.error_fixers['missing_en_key'] == 'off':
+        return extracted_strings
+    else:
+        return missing_en_key.test(extracted_strings)
