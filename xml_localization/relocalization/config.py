a2544bc4bd17cb900ffdafb0225de4935eb181d9
Max Patiiuk <max@patii.uk>
2021-03-11 18:43:45 +0200

    Change indentation from tabs to spaces

--- xml_localization/relocalization/config.py
+++ xml_localization/relocalization/config.py
@@ -12,66 +12,64 @@ working_directory = 'xml_localization/relocalization/workdir'
 # Languages not present in this list would not be modified
 # For each language, specify its HTML attributes
 languages = {
-    'en': {
-        # xml_attributes for each <str> key of that language
-        'xml_attributes': {
-            'language': 'en',
-        },
-        'error_fixers': {
-           # no error fixers are applicable to the 'en' language
-        },
-        # charset of that language
-        # used in regular expressions
-        # case-insensitive
-        'charset': 'a-z',
+  'en': {
+    # xml_attributes for each <str> key of that language
+    'xml_attributes': {
+      'language': 'en',
     },
-    # 'uk': {
-    #     'xml_attributes': {
-    #         'language': 'uk',
-    #         'country': 'UA',
-    #         'variant': '',
-    #     },
-    #     'error_fixers': {
-    #         # Most fixes can be set to the following values
-    #         # 'off' - ignore the issue, if possible
-    #         # 'automatic' - fix, and don't prompt, if possible
-    #         # 'prompt' - ask on each occurrence of this error
-    #
-    #         # Makes sure that the string has the same case as the `en`
-    #         # variant
-    #         'wrong_case': 'automatic',  # 'automatic' / 'prompt' / 'off'
-    #
-    #         # Makes sure that strings have the same prefix and suffix,
-    #         # if any, as the `en` variant. Non letter characters at the
-    #         # start or end of the string are considered a prefix or
-    #         # suffix respectively
-    #         'unmatched_symbol_group': 'automatic',
-    #     },
-    #     'charset': 'іа-яїґь',
-    # },
-    'ru': {
-        'xml_attributes': {
-            'language': 'ru',
-            'country': 'RU',
-            'variant': '',
-        },
-        'error_fixers': {
-            'wrong_case': 'automatic',
-            'unmatched_symbol_group': 'automatic',
-        },
-        'charset': 'а-я',
+    'error_fixers':   {
+      # no error fixers are applicable to the 'en' language
     },
+    # charset of that language
+    # used in regular expressions
+    # case-insensitive
+    'charset':        'a-z',
+  },
+  # 'uk': {
+  #     'xml_attributes': {
+  #         'language': 'uk',
+  #         'country': 'UA',
+  #         'variant': '',
+  #     },
+  #     'error_fixers': {
+  #         # Most fixes can be set to the following values
+  #         # 'off' - ignore the issue, if possible
+  #         # 'automatic' - fix, and don't prompt, if possible
+  #         # 'prompt' - ask on each occurrence of this error
+  #
+  #         # Makes sure that the string has the same case as the `en`
+  #         # variant
+  #         'wrong_case': 'automatic',  # 'automatic' / 'prompt' / 'off'
+  #
+  #         # Makes sure that strings have the same prefix and suffix,
+  #         # if any, as the `en` variant. Non letter characters at the
+  #         # start or end of the string are considered a prefix or
+  #         # suffix respectively
+  #         'unmatched_symbol_group': 'automatic',
+  #     },
+  #     'charset': 'іа-яїґь',
+  # },
+  'ru': {
+    'xml_attributes': {
+      'language': 'ru',
+      'country':  'RU',
+      'variant':  '',
+    },
+    'error_fixers':   {
+      'wrong_case':             'automatic',
+      'unmatched_symbol_group': 'automatic',
+    },
+    'charset':        'а-я',
+  },
 }
-
 
 # Like the `error_fixers` structure inside the `languages` dictionary,
 # but includes global error fixes
 error_fixers = {
-    # Fix strings that don't have the `en` key
-    # This can't be turned off, but can be set to 'prompt'
-    'missing_en_key': 'automatic',
+  # Fix strings that don't have the `en` key
+  # This can't be turned off, but can be set to 'prompt'
+  'missing_en_key': 'automatic',
 }
-
 
 #
 # OPTIONAL CONFIGURATION OPTIONS
@@ -96,6 +94,7 @@ schema_editable_file_name = 'schema_editable.tsv'
 line_separator = '\n'
 column_separator = '\t'
 schema_editable_file_separator = '%s~~~\/\/\/~~~%s' % (
-    line_separator,
-    line_separator
+  line_separator,
+  line_separator
 )
+schema_edited_file_separator = '%s\t\n' % schema_editable_file_separator[0:-1]
--- xml_localization/relocalization/utils.py
+++ xml_localization/relocalization/utils.py
@@ -1,251 +1,260 @@
-import os
-from xml_localization.relocalization import config
-import xmltodict
-import json
-from termcolor import colored
-import re
 import copy
+import json
+import os
+import re
 
+import xmltodict
+from termcolor import colored
+
+from xml_localization.relocalization import config
 
 defined_languages = config.languages.keys()
 
 
 def loop(function):
+  base_dir = os.path.join(
+    os.getcwd(),
+    config.working_directory
+  )
 
-    base_dir = os.path.join(
-        os.getcwd(),
-        config.working_directory
+  for directory in os.listdir(base_dir):
+
+    full_path = os.path.join(
+      base_dir,
+      directory
     )
 
-    for directory in os.listdir(base_dir):
+    if not os.path.isdir(full_path):
+      continue
 
-        full_path = os.path.join(
-            base_dir,
-            directory
-        )
-
-        if not os.path.isdir(full_path):
-            continue
-
-        print(colored(full_path.split('/')[-1],'blue'))
-        function(full_path)
+    print(colored(full_path.split('/')[-1], 'blue'))
+    function(full_path)
 
 
 def parse_xml(working_location):
-    with open(
-            os.path.join(
-               working_location,
-               config.source_file_name
-            )
-    ) as source_file:
-       return xmltodict.parse(source_file.read())
-
+  with open(
+    os.path.join(
+      working_location,
+      config.source_file_name
+    )
+  ) as source_file:
+    return xmltodict.parse(source_file.read())
 
 
 def dump_to_json(working_location, source, file_name):
-    dump_to_file(
-        working_location,
-        json.dumps(
-            source,
-            indent=4,
-            sort_keys=False,
-            ensure_ascii=False
-        ),
-        file_name
-    )
+  dump_to_file(
+    working_location,
+    json.dumps(
+      source,
+      indent=4,
+      sort_keys=False,
+      ensure_ascii=False
+    ),
+    file_name
+  )
 
 
-def dump_to_file(working_location, source:str, file_name):
-    with open(
-        os.path.join(
-            working_location,
-            file_name
-        ),
-        'w'
-    ) as file:
-        file.write(source)
+def dump_to_file(working_location, source: str, file_name):
+  with open(
+    os.path.join(
+      working_location,
+      file_name
+    ),
+    'w'
+  ) as file:
+    file.write(source)
+
+
+def read_from_file(working_location, file_name):
+
+
+def read_from_file(working_location, file_name):
+  with open(os.path.join(
+    working_location,
+    file_name
+  )) as file:
+    return file.read()
 
 
 def extract_string(list_of_str, path, error_callback):
-    extracted_line = {}
+  extracted_line = { }
 
-    if not isinstance(list_of_str, list):
-        list_of_str = [list_of_str]
+  if not isinstance(list_of_str, list):
+    list_of_str = [list_of_str]
 
-    for string in list_of_str:
-        if 'text' in string:
-            extracted_line[string['@language']] = \
-                string['text']
+  for string in list_of_str:
+    if 'text' in string:
+      extracted_line[string['@language']] = \
+        string['text']
 
-    if not len(extracted_line.keys()):
-        return []
+  if not len(extracted_line.keys()):
+    return []
 
-    if 'en' not in extracted_line and error_callback:
-        response = error_callback(
-            'missing_en_key',
-            '%s Strings for English language are missing %s. Location: %s' % (
-                colored('ERROR: ', 'red'),
-                json.dumps(extracted_line, indent=4, sort_keys=False),
-                path
-            )
-        )
+  if 'en' not in extracted_line and error_callback:
+    response = error_callback(
+      'missing_en_key',
+      '%s Strings for English language are missing %s. Location: %s' % (
+        colored('ERROR: ', 'red'),
+        json.dumps(extracted_line, indent=4, sort_keys=False),
+        path
+      )
+    )
 
-        if isinstance(response, str):
-            extracted_line['en'] = response
+    if isinstance(response, str):
+      extracted_line['en'] = response
 
-    for language in defined_languages:
-        if language not in extracted_line:
-            extracted_line[language] = ''
+  for language in defined_languages:
+    if language not in extracted_line:
+      extracted_line[language] = ''
 
-    return [extracted_line]
+  return [extracted_line]
 
 
 def extract_strings(structure, path, error_callback):
-    extracted_strings = []
-    for branch in ['names', 'descs']:
-        if branch in structure and structure[branch] is not None:
-            extracted_strings += extract_string(
-                structure[branch]['str'],
-                '%s > %s' % (
-                    path,
-                    branch
-                ),
-                error_callback
-            )
-    return extracted_strings
+  extracted_strings = []
+  for branch in ['names', 'descs']:
+    if branch in structure and structure[branch] is not None:
+      extracted_strings += extract_string(
+        structure[branch]['str'],
+        '%s > %s' % (
+          path,
+          branch
+        ),
+        error_callback
+      )
+  return extracted_strings
 
 
 def parse_structure(structure, error_callback):
-    extracted_strings = []
+  extracted_strings = []
 
-    containers = structure['vector']['container']
+  containers = structure['vector']['container']
 
-    if not isinstance(containers, list):
-        containers = [containers]
+  if not isinstance(containers, list):
+    containers = [containers]
 
-    for container in containers:
+  for container in containers:
 
-        extracted_strings += extract_strings(
-            container,
-            container['@name'],
-            error_callback
-        )
+    extracted_strings += extract_strings(
+      container,
+      container['@name'],
+      error_callback
+    )
 
-        if 'items' not in container or container['items'] is None:
-            continue
+    if 'items' not in container or container['items'] is None:
+      continue
 
-        list_of_desc = container['items']['desc']
+    list_of_desc = container['items']['desc']
 
-        if not isinstance(list_of_desc, list):
-            list_of_desc = [list_of_desc]
+    if not isinstance(list_of_desc, list):
+      list_of_desc = [list_of_desc]
 
-        for desc in list_of_desc:
-            extracted_strings += extract_strings(
-                desc,
-                '%s > %s' % (
-                    container['@name'],
-                    desc['@name']
-                ),
-                error_callback
-            )
+    for desc in list_of_desc:
+      extracted_strings += extract_strings(
+        desc,
+        '%s > %s' % (
+          container['@name'],
+          desc['@name']
+        ),
+        error_callback
+      )
 
-    return extracted_strings
+  return extracted_strings
 
 
-def dict_to_spreadsheet(structure)->str:
-    languages_to_include = config.languages.keys()
-    spreadsheet = [languages_to_include]
+def dict_to_spreadsheet(structure) -> str:
+  languages_to_include = config.languages.keys()
+  spreadsheet = [languages_to_include]
 
-    for line in structure:
-        spreadsheet_row = []
-        for language in languages_to_include:
-            spreadsheet_row.append(
-                line[language] if language in line
-                else ''
-            )
-        spreadsheet.append(spreadsheet_row)
+  for line in structure:
+    spreadsheet_row = []
+    for language in languages_to_include:
+      spreadsheet_row.append(
+        line[language] if language in line
+        else ''
+      )
+    spreadsheet.append(spreadsheet_row)
 
-    return config.line_separator.join([
-        config.column_separator.join(line) for line in spreadsheet
-    ])
+  return config.line_separator.join([
+    config.column_separator.join(line) for line in spreadsheet
+  ])
 
 
 def prompt(question):
-    response = input('%s ' % question)
-    return response.lower() in ['y','yes', 'true', 't', '1']
+  response = input('%s ' % question)
+  return response.lower() in ['y', 'yes', 'true', 't', '1']
 
 
 substr = lambda string, position: string[position[0]:position[1]]
 
 
 def re_search(regex_pattern, string):
-   match = re.search(regex_pattern, string, re.IGNORECASE)
+  match = re.search(regex_pattern, string, re.IGNORECASE)
 
-   if not match:
-       return ''
+  if not match:
+    return ''
 
-   else:
-       return match.group()
+  else:
+    return match.group()
 
 
 def strip_strings(extracted_strings):
+  stripped_strings = copy.deepcopy(extracted_strings)
+  regex = r'[^%s]+' % config.full_charset
 
-    stripped_strings = copy.deepcopy(extracted_strings)
-    regex = r'[^%s]+' % config.full_charset
+  for index, line in enumerate(stripped_strings):
 
-    for index, line in enumerate(stripped_strings):
+    for position in ['start', 'end']:
+      regex_pattern = ('^%s' if position == 'start'
+                       else '%s$') % regex
 
-        for position in ['start', 'end']:
-            regex_pattern = ('^%s' if position=='start'
-                else '%s$') % regex
+      # match = test(line['en'])
+      #
+      # if not match:
+      #     continue
 
-            # match = test(line['en'])
-            #
-            # if not match:
-            #     continue
+      for language_code, \
+          language_config in config.languages.items():
 
-            for language_code, \
-                language_config in config.languages.items():
+        if language_code not in line:
+          continue
 
-                if language_code not in line:
-                    continue
+        local_match = re_search(
+          regex_pattern,
+          line[language_code]
+        )
 
-                local_match = re_search(
-                    regex_pattern,
-                    line[language_code]
-                )
+        if local_match:
+          stripped_strings[index][language_code] = substr(
+            stripped_strings[index][language_code],
+            [len(local_match), None] if position == 'start'
+            else [0, -len(local_match)]
+          )
 
-                if local_match:
-                    stripped_strings[index][language_code] = substr(
-                        stripped_strings[index][language_code],
-                        [len(local_match), None] if position == 'start'
-                        else [0, -len(local_match)]
-                    )
+        # if local_match != match:
+        #     response = error_callback(
+        #         'unmatched_symbol_group',
+        #         (
+        #             '%s Symbol group of `%s` (%s) differs' +
+        #             'from that of `%s` (en): %s'
+        #         ) % (
+        #             colored('ERROR: ', 'red'),
+        #             line[language_code],
+        #             language_code,
+        #             line['en']
+        #         )
+        #     )
 
-                # if local_match != match:
-                #     response = error_callback(
-                #         'unmatched_symbol_group',
-                #         (
-                #             '%s Symbol group of `%s` (%s) differs' +
-                #             'from that of `%s` (en): %s'
-                #         ) % (
-                #             colored('ERROR: ', 'red'),
-                #             line[language_code],
-                #             language_code,
-                #             line['en']
-                #         )
-                #     )
-
-    return stripped_strings
+  return stripped_strings
 
 
 def unique_strings(striped_strings):
-    distinct_strings_dict = {}
+  distinct_strings_dict = { }
 
-    for line in striped_strings:
-        if line['en'] in distinct_strings_dict:
-            continue
-        else:
-            distinct_strings_dict[line['en']] = line
+  for line in striped_strings:
+    if line['en'] in distinct_strings_dict:
+      continue
+    else:
+      distinct_strings_dict[line['en']] = line
 
-    return list(distinct_strings_dict.values())
+  return list(distinct_strings_dict.values())
