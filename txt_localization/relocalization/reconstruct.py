c8e5d8ff28bd5386ad69f21f37e4cc57d9690d1a
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-11 18:31:13 +0200

    Update txt_localization scripts

--- txt_localization/relocalization/reconstruct.py
+++ txt_localization/relocalization/reconstruct.py
@@ -4,8 +4,18 @@ files = glob.glob(os.path.join(global_base_dir, 'xlsx_to_tsv/*.tsv'))
 file_names = [os.path.basename(file) for file in files]
 file_base_names = [os.path.splitext(file)[0] for file in file_names]
 
+# If true and forth column is not empty:
+#   new strings would be read from the forth column
+# else:
+# 	reads from the second column
+has_forth_column = False
+
+
 
 def process_file(in_file_name, out_file_name):
+
+	print("%s >>> %s" % (in_file_name, out_file_name))
+
 	with open(in_file_name) as file:
 		lines = file.read().split('\n')
 
@@ -20,10 +30,12 @@ def process_file(in_file_name, out_file_name):
 			out_lines.append(split_line[0])
 			continue
 
-		if split_line[3] == '':
+		if has_forth_column and split_line[3] and split_line[3] == '':
 			split_line[3] = split_line[2]
 
-		key_name, new_localized_variant = split_line[0], split_line[3]
+		key_name, new_localized_variant = split_line[0], split_line[
+			2 + int(has_forth_column)
+		]
 		out_lines.append('%s=%s' % (key_name, new_localized_variant))
 
 	with open(out_file_name, 'w') as out_file:
