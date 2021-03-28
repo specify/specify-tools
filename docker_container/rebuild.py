44d0a5d8e9c74352769004c90bf1c86357729115
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-28 16:32:57 +0300

    Add migration code to the container build script

--- docker_container/rebuild.py
+++ docker_container/rebuild.py
@@ -4,7 +4,7 @@ from common import *
 send_notification('Rebuilding...','Rebuilding...')
 
 def run_command():
-        command = "(cd /Users/mambo/site/py_charm/specify7 && docker-compose up --build -d)"
+        command = "(cd /Users/mambo/site/python/specify7 && docker-compose up --build -d)"
         print(command)
         p = subprocess.Popen(command,
 		stdout=subprocess.PIPE,
@@ -26,5 +26,7 @@ for l, rc in run_command():
 	#	send_notification('ERROR!!!','Error occurred while updating Specify 7!','Hero')
 	print(l, end="", flush=True)
 
+run_process("docker exec specify7_specify7_1 mkdir -p '/volumes/static-files/frontend-static/'")
+run_process("docker cp /Users/mambo/site/python/specify7/specifyweb/frontend/static/img specify7_specify7_1:/volumes/static-files/frontend-static/img")
+run_process("docker exec specify7_specify7_1 bash -c './ve/bin/python manage.py migrate auth && ./ve/bin/python manage.py migrate contenttypes && ./ve/bin/python manage.py migrate sessions && ./ve/bin/python manage.py migrate workbench && ./ve/bin/python manage.py migrate notifications'")
 send_notification('Rebuilt...','Rebuilt...', 'Glass')
-run_process('(cd /Users/mambo/site/py_charm/specify7/specifyweb/frontend/static && docker cp ./img specify7_specify7_1:/volumes/static-files/frontend-static/)')
